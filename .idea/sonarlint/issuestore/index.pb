eec704a936c1e36416b9937d8a236e69f6cb8f43
Sanyam Jain <jsanyam@vmware.com>
2024-05-06 11:23:15 +0530

    removed duplicate project

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+w
+GBindingDemo1/app/src/main/java/com/anushka/bindingdemo1/MainActivity.kt,d/6/d652b6e11f5ce0777284100fa3231a77a1aca3f8
--- ViewModelLiveDataDataBinding3/app/src/main/java/com/anushka/twowaydemo1/User.kt
+++
@@ -1,9 +0,0 @@
-package com.anushka.twowaydemo1
-
-import androidx.databinding.BaseObservable
-import androidx.databinding.Bindable
-
-data class User(
-    var name: String,
-    var email: String
-)
