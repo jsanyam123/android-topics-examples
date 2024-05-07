4b7795efd83ea704046a7ade61a690cb0cdca04b
Sanyam Jain <jsanyam@vmware.com>
2024-05-07 22:02:23 +0530

    CoroutineScope + withContext Example

---
+++ Coroutine examples/FinalProject 12/CoroutinesDemo1/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+m
+=app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt,4/e/4e4d3b9e63a048bba5411c8c9c89c6f28d0b362e
+l
+<app/src/main/java/com/sanyam/coroutinesdemo1/MainActivity.kt,7/b/7b02099ccc3be47fff7bf44499f797f756e3b77b
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+~
+Napp/src/androidTest/java/com/sanyam/coroutinesdemo1/ExampleInstrumentedTest.kt,5/8/58922b3dccbe5e75fa52be0e48161a0556817a8f
--- Coroutine examples/FinalProject 12/CoroutinesDemo1/app/src/androidTest/java/com/sanyam/coroutinesdemo1/ExampleInstrumentedTest.kt
+++ Coroutine examples/FinalProject 12/CoroutinesDemo1/app/src/androidTest/java/com/sanyam/coroutinesdemo1/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.anushka.coroutinesdemo1
+package com.sanyam.coroutinesdemo1
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.anushka.coroutinesdemo1", appContext.packageName)
+        assertEquals("com.sanyam.coroutinesdemo1", appContext.packageName)
     }
 }
