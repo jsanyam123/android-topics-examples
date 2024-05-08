f24699e1cb71258fec476500b1c19cb00b65a522
Sanyam Jain <jsanyam@vmware.com>
2024-05-08 12:09:37 +0530

    basic dagger example with module for a dummy 3rd party lib

--- Dependency Injection/2/app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
+++
@@ -1,8 +0,0 @@
-package com.anushka.didemo
-
-import dagger.Component
-
-@Component(modules = [MemoryCardModule::class])
-interface SmartPhoneComponent {
-   fun getSmartPhone() : SmartPhone
-}
---
+++ Dependency Injection/2/app/src/main/java/com/sanyam/didemo/MemoryCardModule.kt
@@ -0,0 +1,13 @@
+package com.sanyam.didemo
+
+import dagger.Module
+import dagger.Provides
+
+@Module
+class MemoryCardModule {
+
+    @Provides
+    fun providesMemoryCard():MemoryCard{
+        return MemoryCard()
+    }
+}
