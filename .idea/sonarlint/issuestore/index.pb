7bc483308bd49027e30b265be75be134f5b98003
Sanyam Jain <jsanyam@vmware.com>
2024-07-15 03:17:57 +0530

    added sample app

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,13 @@
 
 i
 9app/src/main/java/com/example/servicedemo/MainActivity.kt,4/4/4498dd432bd0a03ef1d0a41518e6cf83bac1bc7d
-p
-@app/src/main/java/com/example/servicedemo/MyBackgroundService.kt,3/e/3ea92c0d149cb937a063e5ef32808aa118beaf9f
 P
  app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+?
+settings.gradle,0/5/05efc8b1657769a27696d478ded1e95f38737233
+A
+gradle.properties,2/a/2afbb999f001938c88fa43fc2ef52abf0f8213e4
--- app/src/main/java/com/example/servicedemo/MainActivity.kt
+++ app/src/main/java/com/example/servicedemo/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.servicedemo
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 import com.example.servicedemo.databinding.ActivityMainBinding
 
@@ -13,17 +11,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val serviceIntent = Intent(this, MyBackgroundService::class.java)
-        serviceIntent.putExtra(MyBackgroundService.NAME,"Alex")
-        serviceIntent.putExtra(MyBackgroundService.MARKS,78)
-        binding.btnStart.setOnClickListener {
-           Log.i(MyBackgroundService.TAG,"Starting service")
-           startService(serviceIntent)
-        }
-
-        binding.btnStop.setOnClickListener {
-          Log.i(MyBackgroundService.TAG,"Stopping service")
-          stopService(serviceIntent)
-        }
     }
 }
