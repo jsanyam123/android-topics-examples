b0a093598f0f3c143dbf8e51257afec20c573dc4
Sanyam Jain <jsanyam@vmware.com>
2024-07-14 14:35:27 +0530

    added service class

---
+++ StopWatchDemo/app/src/main/java/com/example/stopwatchdemo/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.stopwatchdemo
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.stopwatchdemo.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var isStarted = false
+    private lateinit var serviceIntent : Intent
+    private var time = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.btnStart.setOnClickListener {
+            startOrStop()
+        }
+        binding.btnReset.setOnClickListener {
+            reset()
+        }
+       serviceIntent = Intent(applicationContext,StopWatchService::class.java)
+
+       registerReceiver(updateTime, IntentFilter(StopWatchService.UPDATED_TIME),
+           RECEIVER_NOT_EXPORTED)
+    }
+
+    private fun startOrStop() {
+        if (isStarted)
+            stop()
+        else
+            start()
+    }
+
+    private fun start() {
+        serviceIntent.putExtra(StopWatchService.CURRENT_TIME,time)
+        startService(serviceIntent)
+        binding.btnStart.text = "Stop"
+        isStarted = true
+    }
+
+    private fun stop() {
+        stopService(serviceIntent)
+        binding.btnStart.text = "Start"
+        isStarted = false
+    }
+
+    private fun reset() {
+        stop()
+        time = 0.0
+        binding.tvTime.text = getFormattedTime(time)
+
+    }
+
+    private val updateTime : BroadcastReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(StopWatchService.CURRENT_TIME,0.0)
+            binding.tvTime.text = getFormattedTime(time)
+        }
+    }
+
+    private fun getFormattedTime(time:Double):String{
+        val timeInt = time.roundToInt()
+        val hours = timeInt % 86400 / 3600
+        val minutes = timeInt % 86400 % 3600 / 60
+        val seconds = timeInt % 86400 % 3600 % 60
+
+        return String.format("%02d:%02d:%02d",hours,minutes,seconds)
+    }
+
+
+}
